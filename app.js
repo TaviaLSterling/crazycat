066388de9162b143cf0156df1e13fa798901dd86
Tavia Sterling <TaviaSterling@outlook.com>
2018-08-02 12:56:50 -0600

    class changes

--- app.js
+++ app.js
@@ -1,46 +1,90 @@
 const mrSnibbly = {
     name: 'Mr. Snibbly',
     moods: [
-        'Classy',
-        'Sassy',
-        'Gone'
+      'Classy',
+      'Sassy',
+      'He Gone!'
+    ],
+    moodImgs: [
+      'assets/cat.jpg',
+      'assets/madcat.jpg',
+      ""
     ],
     tolerance: 7,
     pets: 0,
-    moodIndex: 0,
-
+    moodIndex: 0
+  }
+  const mittens = {
+    name: 'Mittens',
+    moods: [
+      'Sleepy',
+      'Bitey',
+      'She Gone!'
+    ],
     moodImgs: [
-        'assets/cat.jpg',
-        'assets/madcat.jpg'
-    ]
-}
-const catImg = document.getElementById('cat-img')
-const catName = document.getElementById('name')
-const mood = document.getElementById('mood')
-const pets = document.getElementById('pets')
-const petButton = document.getElementById('pet-button')
-function draw() {
-
-    catImg.setAttribute('src', mrSnibbly.moodImgs[mrSnibbly.moodIndex])
-    catName.innerText = mrSnibbly.name
-    mood.innerText = mrSnibbly.moods[mrSnibbly.moodIndex]
-    pets.innerText = mrSnibbly.pets
-    if(mrSnibbly.pets >= mrSnibbly.tolerance*2){
-        document.getElementById('pet-button').disabled = true;
+      'https://petcube.com/blog/content/images/2017/08/kitten-supplies-cover.jpg',
+      'https://www.catster.com/wp-content/uploads/2017/12/A-gray-kitten-meowing.jpg',
+      ""
+    ],
+    tolerance: 2,
+    pets: 0,
+    moodIndex: 0
+  }
+  let activeCat = mrSnibbly
+  
+  let cats = [mrSnibbly, mittens]
+  
+  const catImg = document.getElementById('cat-img')
+  const catName = document.getElementById('name')
+  const mood = document.getElementById('mood')
+  const pets = document.getElementById('pets')
+  const petButton = document.getElementById('pet-button')
+  
+  function draw() {
+    catImg.setAttribute('src', activeCat.moodImgs[activeCat.moodIndex])
+    catName.innerText = activeCat.name
+    mood.innerText = activeCat.moods[activeCat.moodIndex]
+    pets.innerText = activeCat.pets.toString()
+    if (activeCat.pets >= activeCat.tolerance * 2) {
+      petButton.disabled = true;
     }
-}
-
-function pet() {
-    mrSnibbly.pets++;
-    if (mrSnibbly.pets % mrSnibbly.tolerance == 0) {
-        mrSnibbly.moodIndex++;
+  }
+  
+  function pet() {
+    activeCat.pets++;
+    if (activeCat.pets % activeCat.tolerance == 0) {
+      activeCat.moodIndex++
     }
     draw();
-}
-function reset() {
-mrSnibbly.pets = 0;
-mrSnibbly.moodIndex = 0;
-petButton.disabled = false;
-draw();
-}
-draw();
+  }
+  
+  function reset() {
+    activeCat.pets = 0
+    activeCat.moodIndex = 0
+    petButton.disabled = false
+    draw()
+  }
+  
+  
+  function drawButtons() {
+    let template = ""
+    for (let i = 0; i < cats.length; i++) {
+      const cat = cats[i];
+      template += `
+      <button onclick= "setActiveCat(${i})">${cat.name}
+      </button>
+      `
+  
+    }
+    document.getElementById("cat-buttons").innerHTML = template
+  }
+  
+  function setActiveCat(catIndex) {
+    activeCat = cats[catIndex]
+  
+    draw()
+  }
+  
+  
+  drawButtons()
+  draw()
